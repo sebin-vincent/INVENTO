0998be939a0fe58935057642cce8329011f3291c
sebin vincent <sebinvincent@ymail.com>
2018-02-28 02:11:05 +0530

    cse_complted,eee_completed reminder_set:28/02/2018_00:42

--- app/src/main/java/com/example/sebinvincent/invento/acti_dprt_eee.java
+++ app/src/main/java/com/example/sebinvincent/invento/acti_dprt_eee.java
@@ -1,13 +1,27 @@
 package com.example.sebinvincent.invento;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class acti_dprt_eee extends AppCompatActivity {
+
+    int occurance_time=0;                                               //initialized by sebin
+    ImageView menu;
+
+    FragmentTransaction fragmentTransaction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dprt_eee);
+
+        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.Container,new frag_eee_events());
+        fragmentTransaction.commit();
+
     }
 }
--- app/src/main/java/com/example/sebinvincent/invento/frag_eee_events.java
+++ app/src/main/java/com/example/sebinvincent/invento/frag_eee_events.java
@@ -1,7 +1,6 @@
 package com.example.sebinvincent.invento;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -11,51 +10,50 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 
-public class frag_cse_events extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class frag_eee_events extends Fragment {
 
     ImageView competitions;
     ImageView workshops;
     ImageView talks;
 
 
-
-    public frag_cse_events() {
-
+    public frag_eee_events() {
         // Required empty public constructor
-
     }
-
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view;
-        view=inflater.inflate(R.layout.frag_cse_events, container, false);
+        view=inflater.inflate(R.layout.frag_eee_events, container, false);
 
-        competitions=(ImageView)view.findViewById(R.id.cse_comp);
-        workshops=(ImageView)view.findViewById(R.id.cse_wrkshp);
-        talks=(ImageView)view.findViewById(R.id.cse_talksndshow);
+
+        competitions=(ImageView)view.findViewById(R.id.eee_comp);
+        workshops=(ImageView)view.findViewById(R.id.eee_wrkshp);
+        talks=(ImageView)view.findViewById(R.id.eee_talksndshow);
 
         competitions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                frag_cse_comp_list cseComp =new frag_cse_comp_list();
+                frag_eee_comp_list eeeComp =new frag_eee_comp_list();
                 FragmentManager manager=getFragmentManager();
-                manager.beginTransaction().replace(R.id.Container,cseComp,cseComp.getTag()).addToBackStack("").commit();
+                manager.beginTransaction().replace(R.id.Container,eeeComp,eeeComp.getTag()).addToBackStack("").commit();
 
             }
         });
-
 
         workshops.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                frag_cse_work_list csework =new frag_cse_work_list();
+                frag_eee_work_list eeework =new frag_eee_work_list();
                 FragmentManager manager=getFragmentManager();
-                manager.beginTransaction().replace(R.id.Container,csework,csework.getTag()).addToBackStack("").commit();
+                manager.beginTransaction().replace(R.id.Container,eeework,eeework.getTag()).addToBackStack("").commit();
 
 
             }
@@ -69,20 +67,14 @@ public class frag_cse_events extends Fragment {
                 FragmentManager manager=getFragmentManager();
                 manager.beginTransaction().replace(R.id.Container,csetalks,csetalks.getTag()).addToBackStack("").commit();
 
-
             }
         });
 
 
 
+
+
         return view;
     }
-
-
-
-
-
-
-
 
 }
