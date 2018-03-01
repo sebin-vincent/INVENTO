8395d52b3f8bcc290de0edd7c8e4675b4b4b40ee
Manu K Nath <manuknath@gmail.com>
2018-03-01 21:22:24 +0530

    departments completed:1/03/2018

--- app/src/main/java/com/example/sebinvincent/invento/frag_menu.java
+++ app/src/main/java/com/example/sebinvincent/invento/frag_menu.java
@@ -1,11 +1,14 @@
 package com.example.sebinvincent.invento;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -25,6 +28,32 @@ public class frag_menu extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.frag_menu,container, false);
     }
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.cseevents) {
+
+            Intent intent =new Intent(frag_menu.this,.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.notifications) {
+
+            Intent intent =new Intent(main_activiy.this,Notification.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+
+
 
 
 }
