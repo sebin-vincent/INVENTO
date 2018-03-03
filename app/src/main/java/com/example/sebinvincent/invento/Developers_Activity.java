8dc04b34d3c5869bbbc123212f383e359606c4bf
DELL <shijithm7@gmail.com>
2018-03-03 23:29:01 +0530

    about us,developers

---
+++ app/src/main/java/com/example/sebinvincent/invento/Developers_Activity.java
@@ -0,0 +1,13 @@
+package com.example.sebinvincent.invento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Developers_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developers_);
+    }
+}
